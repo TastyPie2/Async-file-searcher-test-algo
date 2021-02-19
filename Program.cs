22f3afa94bbce73b70a952cc4ff2fa60db80054d
TastyPie2 <74426910+TastyPie2@users.noreply.github.com>
2021-02-19 09:20:58 +0100

    ah fuck

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Diagnostics;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Linq;
 
@@ -10,83 +9,71 @@ namespace SearchAlgoTest
 {
     class Program
     {
-        //
-        //In terms of speed this is a downgrade
-        //
-
         //global
         public static int filesCount = 0;
         public static int foldersCount = 0;
         public static string keyWord;
         public static bool specificMode = false;
-        public static List<Thread> threads = new List<Thread>();
-
 
         static void Main()
+        {
+            MainAsync();
+        }
+
+        async static void MainAsync()
         {
             Stopwatch searchTime = new Stopwatch();
 
             Console.WriteLine("Keyword \n-s for specific");
-            
+
             keyWord = Console.ReadLine();
 
             searchTime.Start();
 
-            if(keyWord.ToLower().StartsWith("-s"))
+            if (keyWord.ToLower().StartsWith("-s"))
             {
-                keyWord = keyWord.Remove(0,2).TrimStart();
+                keyWord = keyWord.Remove(0, 2).TrimStart();
 
                 Console.Title = keyWord;
 
                 specificMode = true;
-                
+
             }
- 
+
             string[] drives = Environment.GetLogicalDrives();
+
+            List<Task> tasks = new List<Task>();
 
             Console.WriteLine("==========Results==========");
 
             foreach (string drive in drives)
             {
-                Console.WriteLine("Search started in {0}", drive);
-
-                Thread operation = new Thread(() => searchDir(drive));
-
-                operation.Name = string.Format("WorkerThread {0}", drive);
-
-                operation.Priority = ThreadPriority.Highest;
-
-                operation.Start();
-
-                threads.Add(operation);
+                tasks.Add(searchDirAsync(drive));
             }
-            
-            Thread[] pendingThreads = threads.ToArray();
+
+            Task[] pendingTasks = tasks.ToArray();
 
             bool isDone = false;
 
             while (!isDone)
             {
                 isDone = true;
-                for(int i = 0; i<pendingThreads.Length; i++)
+                for (int i = 0; i < pendingTasks.Length; i++)
                 {
-                    if(pendingThreads[i].IsAlive)
-                    {
-                        isDone = false;
-                    }
+                    pendingTasks[i].Wait();
                 }
             }
 
             searchTime.Stop();
 
             Console.WriteLine("==========Summery==========");
-            Console.WriteLine("Elapsedtime {0}ms \nFilesFound: {1} \nFoldersFound: {2} \nPress enter to close this window", searchTime.ElapsedMilliseconds  ,filesCount, foldersCount);
+            Console.WriteLine("Elapsedtime {0}ms \nFilesFound: {1} \nFoldersFound: {2} \nPress enter to close this window", searchTime.ElapsedMilliseconds, filesCount, foldersCount);
 
             Console.ReadKey();
             Console.ReadLine();
         }
 
-        static void searchDir(string dir)
+        static async Task searchDirAsync(string dir)
         {
             string[] subFolders;
             string[] files;
@@ -95,7 +82,7 @@ namespace SearchAlgoTest
             {
                 if (dir.Split(@"\").Last().Contains(keyWord))
                 {
-                    new Thread(() => writeWithNewThread(dir)).Start();
+                    Console.WriteLine(dir);
                     foldersCount++;
                 }
             }
@@ -103,31 +90,30 @@ namespace SearchAlgoTest
             {
                 if (dir.ToLower().Split(@"\").Last().Contains(keyWord))
                 {
-                    new Thread(() => writeWithNewThread(dir)).Start();
+                    Console.WriteLine(dir);
                     foldersCount++;
                 }
             }
 
             try
             {
-
                 subFolders = Directory.GetDirectories(dir);
                 files = Directory.GetFiles(dir);
-   
+
             }
-            catch(Exception)
+            catch (Exception)
             {
                 return;
             }
             try
             {
 
-                foreach(string folder in subFolders)
+                foreach (string folder in subFolders)
                 {
                     try
                     {
-                        searchDir(folder);
-                        
+                        searchDirAsync(folder);
+
                     }
                     catch
                     { }
@@ -138,7 +124,7 @@ namespace SearchAlgoTest
                     {
                         if (file.Split(@"\").Last().Contains(keyWord))
                         {
-                            new Thread(() => writeWithNewThread(file)).Start();
+                            Console.WriteLine(file);
                             filesCount++;
                         }
                     }
@@ -146,22 +132,16 @@ namespace SearchAlgoTest
                     {
                         if (file.ToLower().Split(@"\").Last().Contains(keyWord))
                         {
-                            //i suspected that writing to console was bottlenecking the rest of the program
-                            new Thread(() => writeWithNewThread(file)).Start();
+                            Console.WriteLine(file);
                             filesCount++;
                         }
                     }
                 }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine(e);
             }
-        }
-
-        static async Task writeWithNewThread(string x)
-        {
-            Console.WriteLine(x);
         }
 
     }
